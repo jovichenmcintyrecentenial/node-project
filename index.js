d9a09043f446ea7a09f950ae8b9dede5c3a53d79
jovi848 <jovi848.mcintyre@gmail.com>
2022-10-25 16:55:41 -0500

    add linting library and add readme

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 /* eslint-disable no-undef */
-//config settings
-var SERVER_NAME = 'care-api'
-var PORT = 5000
-var HOST = '127.0.0.1'
+// config settings
 
-var restify = require('restify')
+const SERVER_NAME = 'care-api';
+const PORT = 5000;
+const HOST = '127.0.0.1';
+
+var restify = require('restify');
 
 //create server
-var server = restify.createServer({name: SERVER_NAME})
+var server = restify.createServer({name: SERVER_NAME});
 
 //list to serve and display avaiblem methods
 server.listen(PORT, HOST, function (){
-  console.log('Server %s listening at %s', server.name, server.url)
+    console.log('Server %s listening at %s', server.name, server.url);
+});
 
-})
 
-
-server.use(restify.fullResponse()).use(restify.bodyParser())
+server.use(restify.fullResponse()).use(restify.bodyParser());
 
 
