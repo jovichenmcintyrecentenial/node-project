c85d414a350506ae1e87c8fe6ec4d19ee4776325
jovi848 <jovi848.mcintyre@gmail.com>
2022-10-28 14:35:19 -0500

    add comments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,18 @@
+//import express
 const express = require('express');
+//create router to append routes to
 const router = express.Router();
+//get handlers from controller
 const {gaurd,login} = require('./../controllers/authenicationController');
 
 
+//declare login route with using login handler from controller
+//this is added before the gaurd route so this api doesn't request a token to work
 router.post('/login', login);
 
+//add gaurd handle to verify token sent in header
 router.use(gaurd);
-
-router.get('/user', (req, res, next) => {
-    console.log('authenticated nice!!!')
-    res.send()
-});
-
+//anything route below this point request a token to function
 
 module.exports.userRoutes = router
 
