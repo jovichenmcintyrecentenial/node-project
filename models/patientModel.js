24f819f4c1f209c882ca045d7791c6c224cbf23c
jovi848 <jovi848.mcintyre@gmail.com>
2022-10-28 03:08:09 -0500

    added patient model

---
+++ models/patientModel.js
@@ -0,0 +1,31 @@
+var mongoose = require ('mongoose');
+
+var patientSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: [true, 'first_name required'],
+    },
+    last_name: {
+        type: String,
+        required: [true, 'last_name required'],
+    },
+    gender: {
+        type: String,
+        required: [true, 'gender required'],
+    },
+    dob: {
+        type: Date,
+        required: [true, 'dob required'],
+    },
+    conditions: {
+        type: String,
+        required: [true, 'conditions required'],
+    },
+    allergies: {
+        type: String,
+        required: [true, 'allergies required'],
+    },
+});
+
+const Patient = mongoose.model('Patient', patientSchema);
+module.exports = Patient;
