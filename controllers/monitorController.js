b5b065ada402d4abc590f61e957394299750d000
jovi848 <jovi848.mcintyre@gmail.com>
2022-10-31 18:44:45 -0500

    add spacing to logging

--- controllers/monitorController.js
+++ controllers/monitorController.js
@@ -2,13 +2,13 @@
 //handler use to help monitor api request and response
 var apiMonitor = async (req, res, next) => {
     //display method POST,DELETE,GET or PUT
-    console.log('['+req.method+'] request received')
+    console.log('\n['+req.method+'] request received\n')
     //display serve and route information
-    console.log(process.env.HOST+':'+process.env.MY_PORT+req.originalUrl)
+    console.log(process.env.HOST+':'+process.env.MY_PORT+req.originalUrl+'\n')
     //display request body
     console.log(isEmpty(req.body)?'':req.body)
 
-    console.log('RESPONSE SENT')
+    console.log('\nRESPONSE SENT\n')
 
     next()
 }
